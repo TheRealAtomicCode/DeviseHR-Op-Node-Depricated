96418e61a920ee6e78fd812ef6d00b6fd34db8fc
Abd Al Qader Baghi <a.baghi@MITT.LY>
2023-08-27 21:47:51 +0200

    completed the get user and get company by id

--- Op_Server/src/Controllers/Company-controllers/Company_Controller.ts
+++ Op_Server/src/Controllers/Company-controllers/Company_Controller.ts
@@ -4,9 +4,53 @@ import { AuthenticatedOpRequestI } from '../../Types/OperatorRequestType';
 import {
   findCompany,
   findUsers,
+  getCompanyById,
 } from '../../Services/Company-services/company-services';
+import { getUserById } from '../../Services/Company-services/admin_company_service';
 
 const companyRouter = Router();
+
+companyRouter.get(
+  '/get-company/:id',
+  auth,
+  async (req: AuthenticatedOpRequestI, res: Response) => {
+    try {
+      const company = await getCompanyById(Number(req.params.id));
+      res.status(200).send({
+        data: company,
+        message: '',
+        success: true,
+      });
+    } catch (err: any) {
+      res.status(400).send({
+        data: null,
+        message: err.message,
+        success: false,
+      });
+    }
+  }
+);
+
+companyRouter.get(
+  '/get-user/:id',
+  auth,
+  async (req: AuthenticatedOpRequestI, res: Response) => {
+    try {
+      const user = await getUserById(Number(req.params.id));
+      res.status(200).send({
+        data: user,
+        message: '',
+        success: true,
+      });
+    } catch (err: any) {
+      res.status(400).send({
+        data: null,
+        message: err.message,
+        success: false,
+      });
+    }
+  }
+);
 
 companyRouter.get(
   '/find-company-and-users',
--- Op_Server/src/Services/Company-services/company-services.ts
+++ Op_Server/src/Services/Company-services/company-services.ts
@@ -1,5 +1,75 @@
 import { prisma } from '../../DB/prismaConfig';
 
+const getCompanyById = async (id: number) => {
+  const company = await prisma.companies.findUniqueOrThrow({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      licence_number: true,
+      account_number: true,
+      logo: true,
+      country: true,
+      main_contact_id: true,
+      created_at: true,
+      updated_at: true,
+      max_users_allowed: true,
+      security_question_one: true,
+      security_question_two: true,
+      security_answer_two: true,
+      expiration_date: true,
+      phone_number: true,
+      added_by: true,
+      users: {
+        select: {
+          id: true,
+          first_name: true,
+          last_name: true,
+          email: true,
+          is_terminated: true,
+          is_verified: true,
+          user_type: true,
+        },
+      },
+    },
+  });
+};
+
+const getuserById = async (id: number) => {
+  const user = await prisma.users.findUniqueOrThrow({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      first_name: true,
+      last_name: true,
+      email: true,
+      is_terminated: true,
+      is_verified: true,
+      user_type: true,
+      created_at: true,
+      updated_at: true,
+      added_by: true,
+      edited_by: true,
+      login_attempt: true,
+      last_login_time: true,
+      last_active_time: true,
+      companies: {
+        select: {
+          id: true,
+          name: true,
+          expiration_date: true,
+        },
+      },
+    },
+  });
+
+  return user;
+};
+
 const findCompany = async (searchTerm: string) => {
   const companies = await prisma.companies.findMany({
     where: {
@@ -56,4 +126,4 @@ const findUsers = async (searchTerm: string) => {
   return users;
 };
 
-export { findCompany, findUsers };
+export { getCompanyById, getuserById, findCompany, findUsers };
