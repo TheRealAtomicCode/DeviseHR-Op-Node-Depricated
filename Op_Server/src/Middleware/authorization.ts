d3b959ef698aaf6f689dbcc6af7bff636b2602ac
Abd Al Qader Baghi <a.baghi@MITT.LY>
2023-08-28 20:48:46 +0200

    added te manager authorization middleware

--- Op_Server/src/Middleware/authorization.ts
+++ Op_Server/src/Middleware/authorization.ts
@@ -12,7 +12,22 @@ const isAdmin = (
     req.decodedUser?.userRole === 'admin'
   )
     return next();
-  throw new Error('Access Denied.');
+  throw new Error('Access Denied, only Admins.');
 };
 
-export { isAdmin };
+const isManager = (
+  req: AuthenticatedOpRequestI,
+  res: Response,
+  next: NextFunction
+) => {
+  if (
+    req.decodedUser?.userRole === 'root' ||
+    req.decodedUser?.userRole === 'sudo' ||
+    req.decodedUser?.userRole === 'admin' ||
+    req.decodedUser?.userRole === 'manager'
+  )
+    return next();
+  throw new Error('Access Denied, only Managers.');
+};
+
+export { isAdmin, isManager };
